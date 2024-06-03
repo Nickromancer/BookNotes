821ad8fab068072e530559c856eedaa81352bde0
Nicholas Hansen <104019466+Nickromancer@users.noreply.github.com>
2024-06-03 15:36:41 +0200

    Add index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+import pg from "pg";
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "bookNotes",
+  password: "Scooby160901",
+  port: 5432,
+});
+
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
