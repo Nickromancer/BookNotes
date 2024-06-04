93f03f01bd970ba210664445663a4eb58fb69eb7
Nicholas Hansen <104019466+Nickromancer@users.noreply.github.com>
2024-06-04 16:02:29 +0200

    Created Static Web, doing SQL integration

--- index.js
+++ index.js
@@ -18,8 +18,40 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("index.ejs");
+let user = "Nicholas";
+
+app.get("/", async (req, res) => {
+  const name = await db.query("SELECT id from users WHERE name = $1", [user]);
+  const id = name.rows[0];
+  const result = await db.query("SELECT * FROM books WHERE user_id = $1", [
+    id.id,
+  ]);
+  let books = [];
+  result.rows.forEach((book) => {
+    books.push(book);
+  });
+  res.render("index.ejs", {
+    listBooks: books,
+  });
+});
+
+app.post("/add", async (req, res) => {
+  const result = await db.query("SELECT id from users WHERE name = $1", [user]);
+  const id = result.rows[0];
+  const isbn = parseInt(req.body.isbn);
+  const rating = parseInt(req.body.rating);
+  const notes = req.body.notes;
+  console.log(isbn);
+  console.log(notes);
+  try {
+    await db.query(
+      "INSERT INTO books (user_id, rating, notes, isbn) VALUES ($1, $2, $3, $4)",
+      [id.id, rating, notes, isbn]
+    );
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.listen(port, () => {
