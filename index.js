9cdf63d412b5aaedd9751392d22e0d5985ea555a
Nicholas Hansen <104019466+Nickromancer@users.noreply.github.com>
2024-06-06 17:00:27 +0200

    CRUD Integrated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import pg from "pg";
-import express from "express";
+import express, { response } from "express";
 import bodyParser from "body-parser";
-
+import axios from "axios";
 const app = express();
 const port = 3000;
 
@@ -18,40 +18,134 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let user = "Nicholas";
+let userId = -1;
+let userName;
 
-app.get("/", async (req, res) => {
-  const name = await db.query("SELECT id from users WHERE name = $1", [user]);
-  const id = name.rows[0];
+async function getUserBooks() {
   const result = await db.query("SELECT * FROM books WHERE user_id = $1", [
-    id.id,
+    userId,
   ]);
   let books = [];
   result.rows.forEach((book) => {
     books.push(book);
   });
+  return books;
+}
+
+async function editBook() {
+  console.log("Hello");
+}
+
+app.get("/", async (req, res) => {
+  const books = await getUserBooks();
   res.render("index.ejs", {
     listBooks: books,
+    name: userName,
   });
 });
 
+app.get("/login", async (req, res) => {
+  try {
+    const name = req.query.name;
+    const password = req.query.password;
+    const result = await db.query(
+      "SELECT id from users WHERE name = $1 AND password = $2",
+      [name, password]
+    );
+    userId = result.rows[0].id;
+    userName = name;
+    console.log(userId);
+    res.render("index.ejs", {
+      name: userName,
+      listBooks: await getUserBooks(),
+    });
+  } catch (err) {
+    res.render("index.ejs", {
+      error: "Your email or password were incorrect.",
+      listBooks: await getUserBooks(),
+    });
+  }
+});
+
+app.get("/signout", (req, res) => {
+  userId = -1;
+  userName = "";
+  res.redirect("/");
+});
+
+app.get("/signup", async (req, res) => {
+  const name = req.query.name;
+  const password = req.query.password;
+  try {
+    let result = await db.query(
+      "INSERT INTO users (name, password) VALUES ($1, $2)",
+      [name, password]
+    );
+    result = await db.query(
+      "SELECT id from users WHERE name = $1 AND password = $2",
+      [name, password]
+    );
+    userId = result.rows[0].id;
+    res.render("index.ejs", {
+      name: name,
+      listBooks: await getUserBooks(),
+    });
+  } catch (err) {
+    res.render("index.ejs", {
+      error: "Name already exists.",
+      listBooks: await getUserBooks(),
+    });
+  }
+});
+
 app.post("/add", async (req, res) => {
-  const result = await db.query("SELECT id from users WHERE name = $1", [user]);
-  const id = result.rows[0];
   const isbn = parseInt(req.body.isbn);
   const rating = parseInt(req.body.rating);
   const notes = req.body.notes;
-  console.log(isbn);
-  console.log(notes);
   try {
+    const response = await axios.get(
+      "https://covers.openlibrary.org/b/isbn/ " + isbn + "-M.jpg?default=false"
+    );
+
     await db.query(
       "INSERT INTO books (user_id, rating, notes, isbn) VALUES ($1, $2, $3, $4)",
-      [id.id, rating, notes, isbn]
+      [userId, rating, notes, isbn]
+    );
+    res.redirect("/");
+  } catch (err) {
+    console.error("Failed to create book:", err.message);
+    res.render("index.ejs", {
+      name: userName,
+      error: "That book can't be found.",
+      listBooks: await getUserBooks(userId),
+    });
+  }
+});
+
+app.get("/delete", async (req, res) => {
+  const isbn = req.query.isbn;
+  try {
+    await db.query(
+      "DELETE FROM books WHERE books.isbn = $1 AND books.user_id = $2",
+      [isbn, userId]
     );
     res.redirect("/");
   } catch (err) {
     console.log(err);
+    res.redirect("/");
   }
+});
+
+app.get("/edit", async (req, res) => {
+  const isbn = req.query.isbn;
+  const rating = req.query.rating;
+  const notes = req.query.notes;
+  await db.query("UPDATE books SET rating = $1, notes = $2 WHERE isbn = $3", [
+    rating,
+    notes,
+    isbn,
+  ]);
+  res.redirect("/");
 });
 
 app.listen(port, () => {
